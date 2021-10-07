a38aa7b0c4ce0e81f0fb3a63ec9698300da98f82
marvin espira <marvinespira@adalabsafrica.com>
2021-10-07 20:25:43 +0300

    options api typescript example

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,42 @@
 <template>
-<div>
-  
-</div>
+  <div class="app">
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+    <button @click="changeName('Marvin')">Change Name</button>
+    <button @click="changeAge('30')">Change Age Type String</button>
+    <button @click="changeAge(30)">Change Age Type Number</button>
+  </div>
 </template>
 
 <script lang="ts">
+
+// casting age variable as both string or number
+// let age: string | number = 25
+
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {} 
+  components: {},
+  data(){
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  }, 
+  methods: {
+    changeName(name: string){
+      this.name = name
+      return name
+    },
+    changeAge(age: string | number){
+      this.age = age
+      return age
+    }
+  }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
