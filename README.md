214d139a32f2a14b3278147b8d8b8a6717319232
marvin espira <marvinespira@adalabsafrica.com>
2021-10-07 22:50:58 +0300

    final updates

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# online-jobs
+# Typescript Vuejs 3 online-jobs App
+
+### This covers various Typescript Features including
+
+### 1. Type Definitions such as string, number, boolean, any
+### 2. Generics indicated by <>
+### 3. Creating Interfaces 
+### 4. Creating Custom Types eg the OrderTerm type that accepts only three items (location, title, salary)
+### 5. Types with Props and also PropType
+### 6. Types with Functions
+### 7. Types with Computed Properties
+### 8. It covers Typescript for both Composition and Options APIs
+
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo">
+        <h1>Online Jobs</h1>
+      </div>
       <div class="order">
         <button @click="handleClick('title')">Order by title</button>
         <button @click="handleClick('salary')">Order by salary</button>
@@ -60,5 +64,16 @@ export default defineComponent({
     border-radius: 4px;
     cursor: pointer;
     font-weight: bold;
+  }
+  header .title{
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
   }
 </style>
