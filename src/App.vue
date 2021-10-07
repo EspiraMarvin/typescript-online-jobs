6e45b538074c1e3cb513ae965d5d0f297054e57b
marvin espira <marvinespira@adalabsafrica.com>
2021-10-07 20:36:04 +0300

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="app">
-    <p>{{ name }}</p>
-    <p>{{ age }}</p>
-    <button @click="changeName('Marvin')">Change Name</button>
-    <button @click="changeAge('30')">Change Age Type String</button>
-    <button @click="changeAge(30)">Change Age Type Number</button>
   </div>
 </template>
 
@@ -17,22 +12,7 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {},
-  data(){
-    return {
-      name: 'Link',
-      age: 25 as number | string
-    }
-  }, 
-  methods: {
-    changeName(name: string){
-      this.name = name
-      return name
-    },
-    changeAge(age: string | number){
-      this.age = age
-    }
-  }
+  components: {}
 });
 </script>
 
